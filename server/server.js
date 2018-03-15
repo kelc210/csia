878eb80a237cb58c74a3a66626f4b64d0b705cbd
Kirby <kirbyezra@gmail.com>
2018-03-15 21:13:04 +0800

    fixed dir issues

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
+const path = require('path');
 const express = require('express');
-const hbs = require('hbs');
 
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine','hbs');
+app.use(express.static(publicPath));
 
-app.use(express.static(__dirname + '/public'));
-
-app.listen(port,() =>{
-  console.log(`Server is up on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
